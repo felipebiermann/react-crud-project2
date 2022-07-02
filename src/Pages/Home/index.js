3d2f2bd49616b16f01b26db5629bfbf7a7f1f687
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-07-02 00:47:32 -0300

    first commit, BORA!

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,8 @@
+// import { Link } from "react-router-dom";
+// import { Cards } from "../../components/Cards";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+
+export function Home() {
+  return <h1>Testando</h1>;
+}
