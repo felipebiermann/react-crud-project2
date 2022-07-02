440fcabbdf1f95e8e9eca6c8e7ceb514198c5888
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-07-02 01:54:51 -0300

    btn login criado na home.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./Pages/Home";
+import { LoginPage } from "./Pages/LoginPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login-page" element={<LoginPage />} />
       </Routes>
     </>
   );
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,68 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import { Cards } from "../../components/Cards";
 // import axios from "axios";
 // import { useEffect, useState } from "react";
 
 export function Home() {
-  return <h1>Testando</h1>;
+  return (
+    <>
+      <nav
+        className="navbar navbar-dark bg-dark"
+        style={{
+          color: "white",
+          backgroundColor: "rgb(25, 33, 41)",
+          padding: "0px 0 14px 0",
+        }}
+      >
+        <ul
+          className="nav-bar-ul"
+          style={{
+            width: "1200px",
+            margin: "0 auto",
+            marginLeft: "90%",
+
+            fontSize: "10px",
+          }}
+        >
+          <li
+            className="nav-bar-li"
+            style={{
+              float: "left",
+              listStyleType: "none",
+              textAlign: "center",
+              width: "33%",
+            }}
+          >
+            <Link
+              to="/login-page"
+              className="nav-bar-item"
+              style={{
+                color: "white",
+                fontSize: "2em",
+                textDecoration: "none",
+              }}
+            >
+              Login
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div
+        className="container, navbar navbar-dark bg-dark"
+        style={{ textAlign: "center" }}
+      >
+        <h1
+          style={{
+            margin: "0px",
+            fontFamily: "Blippo, fantasy",
+            letterSpacing: "2px",
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          TESTANDO
+        </h1>
+      </div>
+    </>
+  );
 }
